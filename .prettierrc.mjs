f4987a46204edeeef52f5e630b9cdca45cb74d1f
Bence Bagi <bagi.bence94@gmail.com>
2025-12-30 18:43:57 +0100

    Add prettierrc

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  tabWidth: 2,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
